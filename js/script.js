80e6501201d40cb32d78d140f17d3497eba73bba
xintu7 <7xintu@gmail.com>
2017-03-26 18:24:13 -0400

    1.0

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Navbar Box Shadow on Scroll 
+$(function(){
+    var navbar = $('.navbar');
+    $(window).scroll(function(){
+        if($(window).scrollTop() <= 10){
+       		navbar.css('box-shadow', 'none');
+        } else {
+          navbar.css('box-shadow', '0px 10px 20px rgba(0, 0, 0, 0.4)'); 
+        }
+    });
+});
+
