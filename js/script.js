722c50b89a962c23686219fb1ea5aca9d6465b7c
xintu7 <7xintu@gmail.com>
2017-04-08 23:38:17 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,24 +47,21 @@ $(function () {
 //        console.log( $this.height() );
 //    });
 //}
-$(document).onload(function() {
-    $("#card-explain").load(function() {
-        var h = $(this).contents().find("#card-front").height() - $(this).contents().find("#card-instr").height() - 20
-        $(this).height(h);
-//             var h = $(this).contents().find("ul.jq-text").height();
-//             h += $(this).contents().find("#form1").height();
-//             $(this).height( h );
-        });
-});
-console.log($("#card-front").height() )
 
-$(window).resize(function(){
-    if( $(window).width() > 767){
-        var height = $("#card-front").height() - $("#card-instr").height() - 20
-        $('#card-explain').height(height);
-    }
-})
-$(window).trigger('resize');
+
+    if(!window.location.hash) {
+            window.location = window.location + '#loaded';
+            window.location.reload();
+        }else{
+            $(window).resize(function(){
+                if( $(window).width() > 767){
+                    var height = $("#card-front").height() - $("#card-instr").height() - 20
+                    $('#card-explain').height(height);
+                }
+            })
+            $(window).trigger('resize');
+}
+
 
 
 
