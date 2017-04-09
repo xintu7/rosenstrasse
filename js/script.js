50c17451c7ae0c6db18d8379d75ae84b1f2ed2fa
xintu7 <7xintu@gmail.com>
2017-04-08 23:47:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,14 +47,15 @@ $(function () {
 //        console.log( $this.height() );
 //    });
 //}
-$(document).ready(function() {
-        if (document.readyState == 'complete')
+if (document.readyState == 'complete')
             doOnLoad();
     $(window).bind("load", doOnLoad);
-        }else{
-            if( $(window).width() > 767){
-        var height = $("#card-front").height() - $("#card-instr").height() - 20
-        $('#card-explain').height(height);
+
+
+$(document).ready(function() {
+    if( $(window).width() > 767){
+    var height = $("#card-front").height() - $("#card-instr").height() - 20
+    $('#card-explain').height(height);
         }
 //     $("#card-explain").load(function() {
 //         var h = $(this).contents().find("#card-front").height() - $(this).contents().find("#card-instr").height() - 20
