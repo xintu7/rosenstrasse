64eee48c753ca63decd7fecea8ac95834e7cf747
xintu7 <7xintu@gmail.com>
2017-04-08 23:59:37 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//Navbar Box Shadow on Scroll 
+$(function(){
+    var navbar = $('.navbar');
+    $(window).scroll(function(){
+        if($(window).scrollTop() <= 10){
+       		navbar.css('box-shadow', 'none');
+        } else {
+          navbar.css('box-shadow', '0px 10px 20px rgba(0, 0, 0, 0.4)'); 
+        }
+    });
+});
+
+$(function () {
+    $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+
+$(window).resize(function(){
+    if( $(window).width() > 767){
+        var height = $("#card-front").height() - $("#card-instr").height() - 20
+        $('#card-explain').height(height);
+    }
+    $(window).resize(function() {
+     if( $(window).width() > 767){
+        var height = $("#card-front").height() - $("#card-instr").height() - 20
+        $('#card-explain').height(height);
+    }
+    });$(window).resize();
+});
