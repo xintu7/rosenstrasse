8543322b94614160d127ef4b8bc81b4b5e48de86
xintu7 <7xintu@gmail.com>
2017-04-13 23:20:53 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,8 @@ $(function () {
 });
 
 
-$(window).resize(function(){
+
+$(window).ready(function(){
     if( $(window).width() > 767){
         var height = $("#card-front").height() - $("#card-instr").height() - 20
         $('#card-explain').height(height);
@@ -35,4 +36,38 @@ $(window).resize(function(){
     }
     });$(window).resize();
 });
+
+
+
+   //Get text from Json
+var newCardinstr = $.getJSON("https://raw.githubusercontent.com/xintu7/rosenstrasse/master/js/text.json", function(cardInstr) {
+    var newCardinstr = cardInstr[1].instr;
+    console.log(newCardinstr);
+   });
+
+
+function change(card) {
+  //Get the index of the new card you want to display
+  var cardIndex = card.getAttribute("id");
  
+  //Get the file link to the new left and right images
+  var newCardLeft = "images/cards/l"+cardIndex+".png";
+  var newCardRight = "images/cards/r"+cardIndex+".png";
+    
+  //text//
+    
+  //Find the card back image element and change image src
+  var cardBack = $(".back");
+  cardBack.attr('src', newCardLeft);
+ 
+  //Find the card front image element and change image src
+  var cardFront = $(".front");
+  cardFront.attr('src', newCardRight);
+    
+  //Update the index of the left and right arrows
+  var index = parseInt(cardIndex);
+  var prev = index-1;
+  var next = index+1;
+  $(".left-button").attr('id', prev);
+  $(".right-button").attr('id', next);
+};
